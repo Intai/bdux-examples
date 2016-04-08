dd6e1c4fa91cd2d8db93ed9135225c045b5e7f88
Intai Huang <intai.huang@vodafone.com>
2016-04-08 14:33:57 +1200

    Reset location history for every request

--- isomorphic/app/roots/server-default.js
+++ isomorphic/app/roots/server-default.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import App from '../components/app-react.js';
-import { createLocationHistory} from 'bdux-react-router';
+import { resetLocationHistory} from 'bdux-react-router';
 
 export const createDefaultApp = (req) => {
-  createLocationHistory(req.path);
+  resetLocationHistory(req.path);
   return (<App />);
 };
