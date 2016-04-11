e0fddb8edcec69c9591b7cadced30c623200db48
Intai Huang <intai.huang@vodafone.com>
2016-04-11 15:28:47 +1200

    Reset location-history to render on server

--- isomorphic/app/roots/default-root.js
+++ isomorphic/app/roots/default-root.js
@@ -2,11 +2,11 @@ import React from 'react';
 import App from '../components/app-react';
 import MessageAction from '../actions/message-action';
 import MessageStore from '../stores/message-store';
-import { LocationAction, LocationStore } from 'bdux-react-router';
+import { resetLocationHistory, LocationStore } from 'bdux-react-router';
 import { createRoot } from 'bdux-isomorphic';
 
 export const createElement = (req) => {
-  LocationAction.replace(req.path);
+  resetLocationHistory(req.path);
   MessageAction.message('Server');
   return (<App />);
 };
