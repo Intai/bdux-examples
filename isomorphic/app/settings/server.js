6256261037bb37443270aff46cbd51c61c68856a
intai <intai.hg@gmail.com>
2016-04-12 21:45:13 +1200

    Add logger middleware to server render

--- isomorphic/app/settings/server.js
+++ isomorphic/app/settings/server.js
@@ -1,6 +1,8 @@
+import * as Logger from 'bdux-logger';
 import * as Isomorphic from 'bdux-isomorphic';
 import { applyMiddleware } from 'bdux';
 
 applyMiddleware(
-  Isomorphic
+  Isomorphic,
+  Logger
 );
