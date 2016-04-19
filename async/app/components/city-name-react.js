3b63ffa861ca48b93ff8a73522a407662d090cbc
intai <intai.hg@gmail.com>
2016-04-19 22:42:22 +1200

    Style country-codes and city-name component

--- async/app/components/city-name-react.js
+++ async/app/components/city-name-react.js
@@ -1,5 +1,6 @@
 import R from 'ramda';
 import React from 'react';
+import CountryCodesStore from '../stores/country-codes-store';
 import WeatherStore from '../stores/weather-store';
 import WeatherAction from '../actions/weather-action';
 import classNames from 'classnames/bind';
@@ -12,21 +13,59 @@ const onChange = (event) => {
   WeatherAction.setCity(event.target.value);
 };
 
+const hasCountryAndWeather = (country, weather) => (
+  country && country.selected &&
+  weather && weather.city
+);
+
+const searchWeather = R.converge(
+  WeatherAction.searchWeather, [
+    R.prop('selected'),
+    R.pipe(R.nthArg(1), R.prop('city'))
+  ]
+);
+
+const searchWeatherSafely = R.ifElse(
+  hasCountryAndWeather,
+  searchWeather,
+  R.always()
+);
+
+const preventDefault = R.invoker(0,
+  'preventDefault'
+);
+
+const onSearch = R.curryN(3, R.juxt([
+  searchWeatherSafely,
+  R.pipe(R.nthArg(2), preventDefault)
+]));
+
 const getCity = R.path(
   ['city']
 );
 
-export const CityName = ({ weather }) => (
-  <div>
-    <input value={ getCity(weather) }
-      className={ cssModule({
-        'input': true }) }
-      onChange={ onChange } />
+export const CityName = ({ country, weather }) => (
+  <form onSubmit={ onSearch(country, weather) }
+    className={ cssModule({
+      'wrap': true }) }>
 
-    <button>Go</button>
-  </div>
+    <label className={ cssModule({
+      'label': true }) }>City
+    </label>
+
+    <input value={ getCity(weather) }
+      onChange={ onChange }
+      className={ cssModule({
+        'input': true }) } />
+
+    <button onClick={ onSearch(country, weather) }
+      className={ cssModule({
+        'button': true }) }>Go
+    </button>
+  </form>
 );
 
 export default createComponent(CityName, {
+  country: CountryCodesStore,
   weather: WeatherStore
 });
