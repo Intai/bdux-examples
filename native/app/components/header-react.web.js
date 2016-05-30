ac1c0e3a40fc8c8a6915626fa6920c1e2c945791
intai <intai.hg@gmail.com>
2016-05-30 18:12:49 +1200

    Style header component for web

--- native/app/components/header-react.web.js
+++ native/app/components/header-react.web.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import classNames from 'classnames/bind'
+import styles from './header-style.web.scss'
 import { createComponent } from 'bdux'
 
+const cssModule = classNames.bind(styles);
+
 export const Header = ({ children }) => (
-  <span>{ children }</span>
+  <span className={ cssModule({
+      'label': true }) }>
+    { children }
+  </span>
 )
 
 export default createComponent(Header)
