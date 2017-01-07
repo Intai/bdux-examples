f3d1c7191afbef8d5660b87f6d09cf5f6447cd84
intai <intai.hg@gmail.com>
2017-01-07 23:00:25 +1300

    Update dependencies for universal

--- universal/gulpfile.js
+++ universal/gulpfile.js
@@ -1,4 +1,4 @@
-'use strict';
+/* eslint-env node */
 
 var gulp = require('gulp'),
     gls = require('gulp-live-server'),
@@ -6,7 +6,6 @@ var gulp = require('gulp'),
     webpackStream = require('webpack-stream'),
     WebpackDevServer = require('webpack-dev-server'),
     $ = require('gulp-load-plugins')(),
-    files = './!(dist|build|node_modules)/**/*',
     port = process.env.PORT || 8080;
 
 gulp.task('image', function() {
@@ -18,7 +17,7 @@ gulp.task('clean', function () {
   require('del').sync('dist');
 });
 
-gulp.task('dev-server', function(callback) {
+gulp.task('dev-server', function(_callback) {
   new WebpackDevServer(webpack(require('./webpack/dev.config.js')), {
     historyApiFallback: true,
     noInfo: true,
@@ -42,7 +41,7 @@ gulp.task('build-server', function() {
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('prod-server', function(cb) {
+gulp.task('prod-server', function(_callback) {
   var server = gls(['dist/server.js'], {
     env: process.env
   }, false);
