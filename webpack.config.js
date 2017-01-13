d174f2f030903c87f520a781576e693d06a582b0
intai <intai.hg@gmail.com>
2017-01-14 11:04:27 +1300

    Init infinite-scroll example

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+/* eslint-env node */
+
+var path = require('path'),
+    webpack = require('webpack'),
+    autoprefixer = require('autoprefixer');
+
+module.exports = {
+  context: path.join(__dirname, 'app'),
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/dev-server',
+    './main'
+  ],
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    preLoaders: [{
+      test: /\.jsx?$/,
+      loader: 'eslint-loader',
+      exclude: /node_modules/
+    }],
+    loaders: [{
+      test: /\.jsx?$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/
+    }, {
+      test: /\.scss$/,
+      loaders: [
+        'style',
+        'css?modules&importLoaders=2&localIdentName=[name]__[local]___[hash:base64:5]',
+        'postcss',
+        'sass'
+      ]
+    }]
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  postcss: [
+    autoprefixer
+  ],
+  eslint: {
+    configFile: '.eslintrc'
+  }
+};
