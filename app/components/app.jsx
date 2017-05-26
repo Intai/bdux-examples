e97a6265b9a1937cdca15eca23e907179cfec9d8
intai <intai.hg@gmail.com>
2017-05-27 10:29:19 +1200

    Upgrade universal to v4

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -1,26 +1,25 @@
-import R from 'ramda'
 import React from 'react'
-import Routes from '../routes'
-import { Router, createLocationHistory } from 'bdux-react-router'
+import Page from './page'
+import { Router, Route, createLocationHistory } from 'bdux-react-router'
 import { LocationAction, LocationStore } from 'bdux-react-router'
 import { UniversalStates } from 'bdux-universal'
 import { TimeTravel } from 'bdux-timetravel'
 import { createComponent } from 'bdux'
 
-const renderRouterWithLocation = (location) => (
-  <Router history={ createLocationHistory(location) }
-    routes={ Routes } />
-)
-
-const renderRouter = R.ifElse(
-  R.identity,
-  renderRouterWithLocation,
-  R.F
+const renderRouter = (location) => (
+  !!location && (
+    <Router history={createLocationHistory(location)}>
+      <Route
+        component={Page}
+        path="/:id?"
+      />
+    </Router>
+   )
 )
 
 export const App = ({ location }) => (
   <div>
-    { renderRouter(location) }
+    {renderRouter(location)}
     <UniversalStates />
     <TimeTravel />
   </div>
--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -25,6 +25,9 @@ module.exports = {
       }
     })
   ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
   module: {
     preLoaders: [{
       test: /\.jsx?$/,
