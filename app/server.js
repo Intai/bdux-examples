dddad4e4ef80f1c5452e253fb15855ba58592720
intai <intai.hg@gmail.com>
2018-03-02 12:24:56 +1300

    Upgrade to react 16

--- app/server.js
+++ app/server.js
@@ -1,23 +1,43 @@
 /* eslint-env node */
 
-import './settings';
-import R from 'ramda';
-import Express from 'express';
-import DefaultRoot from './roots/default-root';
+import './settings'
+import fs from 'fs'
+import * as R from 'ramda'
+import Express from 'express'
+import DefaultRoot from './roots/default-root'
 
-const app = Express();
-const port = process.env.PORT || 8080;
+const app = Express()
+const port = process.env.PORT || 8080
 
-app.set('view engine', 'ejs');
-app.set('views', 'dist');
+/*
+app.set('view engine', 'ejs')
+app.set('views', 'dist')
 
 const renderApp = R.curry((root, req, res) => {
   res.render('server', {
     app: root.renderToString(req, res)
-  });
-});
+  })
+})
+*/
 
-app.use('/static', Express.static('dist'));
-app.get('*', renderApp(DefaultRoot));
+const renderApp = R.curry((root, req, res) => {
+  fs.readFile('./dist/server.ejs', 'utf8', (err, file) => {
+    if (err) {
+      throw err
+    }
 
-app.listen(port);
+    const [head, tail] = file.split('<%- app %>')
+    res.write(head)
+    const stream = root.renderToNodeStream(req, res)
+    stream.pipe(res, { end: false })
+    stream.on('end', () => {
+      res.write(tail)
+      res.end()
+    })
+  })
+})
+
+app.use('/static', Express.static('dist'))
+app.get('*', renderApp(DefaultRoot))
+
+app.listen(port)
