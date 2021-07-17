1db1f337f843cc7eb9a1e353bd3ab81dd394404c
intai <intai.hg@gmail.com>
2021-07-17 18:24:21 +1200

    Update pouchdb dependencies

--- pouchdb/gulpfile.js
+++ pouchdb/gulpfile.js
@@ -1,23 +1,19 @@
 /* eslint-env node */
 
 var gulp = require('gulp'),
+    log = require('fancy-log'),
+    PluginError = require('plugin-error'),
     gls = require('gulp-live-server'),
     webpack = require('webpack'),
     webpackStream = require('webpack-stream'),
     WebpackDevServer = require('webpack-dev-server'),
-    PluginError = require('plugin-error'),
-    log = require('fancy-log'),
     fs = require('fs'),
-    sequence = require('gulp-sequence'),
     spawn = require('child_process').spawn,
     port = process.env.PORT || 8080;
 
-gulp.task('clean', function () {
-  require('del').sync('dist');
-});
-
-gulp.task('dev-server', function() {
+function dev() {
   new WebpackDevServer(webpack(require('./webpack/dev.config.js')), {
+    disableHostCheck: true,
     historyApiFallback: true,
     noInfo: true,
     hot: true
@@ -26,37 +22,37 @@ gulp.task('dev-server', function() {
     if (err) throw new PluginError('webpack-dev-server', err);
     log('[webpack-dev-server]', 'http://localhost:' + port);
   });
-});
+}
 
-gulp.task('build-client', function() {
-  return gulp.src('app/main.js')
+function buildClient() {
+  return gulp.src('app/index.jsx')
     .pipe(webpackStream(require('./webpack/client.config.js'), webpack))
     .pipe(gulp.dest('dist'));
-});
+}
 
-gulp.task('build-server', function() {
+function buildServer() {
   return gulp.src('app/server.js')
     .pipe(webpackStream(require('./webpack/server.config.js'), webpack))
     .pipe(gulp.dest('dist'));
-});
+}
 
-gulp.task('prod-server', function() {
-  var server = gls(['dist/server.js'], {
+function prod() {
+  var server = gls('dist/server.js', {
     env: process.env
   }, false);
 
   server.start();
-  log('[express] http://localhost:' + port);
-});
+  log('[express]', 'http://localhost:' + port);
+}
 
-gulp.task('init-pouchdb', function() {
+function initPouchdb() {
   if (!fs.existsSync('pouchdb')) {
     return gulp.src('pouchdb_init/**/*')
       .pipe(gulp.dest('pouchdb'));
   }
-})
+}
 
-gulp.task('pouchdb-server', function() {
+function pouchdbServer() {
   spawn('./node_modules/pouchdb-server/bin/pouchdb-server', [
     '--port', '5984',
     '--host', '0.0.0.0',
@@ -65,26 +61,22 @@ gulp.task('pouchdb-server', function() {
   ], {
     stdio: 'inherit'
   });
-});
+}
 
-gulp.task('build', [
-  'build-client',
-  'build-server'
-]);
+const server = gulp.series(
+  buildClient,
+  buildServer,
+  prod,
+  initPouchdb,
+  pouchdbServer
+)
 
-gulp.task('server', sequence(
-  'build',
-  'prod-server',
-  'init-pouchdb',
-  'pouchdb-server'
+gulp.task('server', server);
+
+gulp.task('dev', gulp.series(
+  dev,
+  initPouchdb,
+  pouchdbServer
 ));
 
-gulp.task('dev', sequence(
-  'dev-server',
-  'init-pouchdb',
-  'pouchdb-server'
-));
-
-gulp.task('default', [
-  'server'
-]);
+gulp.task('default', server);
